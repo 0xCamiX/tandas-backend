5a588a8d7f50cf87d239433d6fbf4ff8d353a82f
0xCamix <juancamilogallego70@icloud.com>
2025-11-08 22:39:43 -0500

    chore: update .gitignore to exclude generated Prisma files, modify biome.json to exclude generated directories, and add @prisma/extension-accelerate dependency in package.json and bun.lock

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { type Express } from "express";
+import { auth } from "./lib/auth";
 
 const app: Express = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
 
 app.get("/", (_req, res) => {
 	res.send("Hello World");
