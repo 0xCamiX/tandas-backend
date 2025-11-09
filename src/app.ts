d033a6f3d8a4fd056eb01479371a33e92be66fb2
0xCamix <juancamilogallego70@icloud.com>
2025-11-08 22:55:47 -0500

    chore: add morgan middleware for logging and update dependencies in package.json and bun.lock

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { toNodeHandler } from "better-auth/node";
 import express, { type Express } from "express";
+import morgan from "morgan";
 import { auth } from "./lib/auth";
 
 const app: Express = express();
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.get("/", (_req, res) => {
 	res.send("Hello World");
